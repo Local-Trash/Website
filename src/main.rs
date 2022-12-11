6e4e9f352fd1c3a0e03c963405408df7f6181478
Local-Trash <tqdagboy@gmail.com>
2022-12-10 21:00:08 -0800

    making the website terminal type

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,11 @@ use yew::prelude::*;
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-            println!("{}", value);
-        }
-    };
+
 
     html! {
         <>
-            <div>
-                <p>{"Test"}</p>
-            </div>
-
-            <div>
-                <button {onclick}>{ "+1" }</button>
-                <p>{ *counter }</p>
-            </div>
-
+            <p id = {classes!("mainText")}>{ "Welcome to the offical website of KeyCap Studio's" }</p>
         </>
     }
 }
