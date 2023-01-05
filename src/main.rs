744f51a5bb8486159bbca6d9f2db824ec7cac460
Local-Trash <tqdagboy@gmail.com>
2023-01-04 20:06:23 -0800

    some small updates

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,58 @@ use yew::prelude::*;
 use yew_router::prelude::*;
 mod pages;
 
+// The pages
 #[derive(Clone, Routable, PartialEq)]
-enum Route {
+enum Route { 
     #[at("/")]
     Home,
+    #[at("/team")]
+    Team,
+    #[at("/games")]
+    Games,
+    #[at("/blog")]
+    Blog,
     #[not_found]
     #[at("/404")]
     Notfound,
 }
 
+// Access to the pages
 fn switch(routes: Route) -> Html {
     match routes {
-        Route::Home => pages::home::site(),
-        Route::Notfound => pages::notFound::site(),
+        Route::Home => pages::home::Site(),
+        Route::Team => pages::team::Site(),
+        Route::Games => pages::games::Site(),
+        Route::Blog => pages::blog::Site(),
+        Route::Notfound => pages::notFound::Site(),
     }
 }
 
+// The header component
 #[function_component(Header)]
 fn header() -> Html {
     html! {
-        <div class={classes!("PopOut")}>
-            {"test"}
+        <div class={classes!("Header")}>
+            <p>{"KeyCap Studios "} <a href="/"><img src="logo.svg" alt="logo"/></a></p>
         </div>
     }
 }
 
+// The main app
 #[function_component]
 fn App() -> Html {
     html! {
-        <BrowserRouter>
-            <Switch<Route> render={switch} />
-        </BrowserRouter>
+        <>
+            <Header/>
+
+            <BrowserRouter>
+                <Switch<Route> render={switch} />
+            </BrowserRouter>
+        </>
     }
 }
 
+// The render of the main app
 fn main() {
     yew::Renderer::<App>::new().render();
 }
--- src/pages.rs
+++ src/pages.rs
@@ -1,10 +1,52 @@
 #![allow(non_snake_case)]
 pub mod home {
-    use yew::{Html, html};
+    use yew::{Html, html, classes};
 
-    pub fn site() -> Html {
+    pub fn Site() -> Html {
         html! {
-            <p>{"help"}</p>
+            <div class={classes!("Home")}>
+                <p class={classes!(vec!["Card"])}>
+                    {"
+                        KeyCap Studios is a group of developers that program and publish digital applications. Our founder and lead developer is @LocalTrash_.
+                    "}
+                </p>
+            </div>
+        }
+    }
+}
+
+pub mod team {
+    use yew::{Html, html, classes};
+
+    pub fn Site() -> Html {
+        html! {
+            <div class={classes!("Team")}>
+
+            </div>
+        }
+    }
+}
+
+pub mod games {
+    use yew::{Html, html, classes};
+
+    pub fn Site() -> Html {
+        html! {
+            <div class={classes!("Blog")}>
+                <title>{"Test"}</title>
+            </div>
+        }
+    }
+}
+
+pub mod blog {
+    use yew::{Html, html, classes};
+
+    pub fn Site() -> Html {
+        html! {
+            <div class={classes!("blog")}>
+
+            </div>
         }
     }
 }
@@ -12,7 +54,7 @@ pub mod home {
 pub mod notFound {
     use yew::{Html, html, classes};
 
-    pub fn site() -> Html {
+    pub fn Site() -> Html {
         html! {
             <div class={classes!("notFound")}>
                 <p>{"Sorry that page was not found?"}</p>
