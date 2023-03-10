a6c51288f9372607af0ccce398608ac6c7d397cf
Loc@lTrash <105620888+Local-Trash@users.noreply.github.com>
2023-03-10 22:03:10 +0000

    started the project

--- README.md
+++
@@ -1,3 +0,0 @@
-### KeyCap Studio Website
-## About 
-This is the offical website of KeyCap Studio. This page has the games that KeyCap Studio has published.
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,31 @@
-use yew::prelude::*;
-use yew_router::prelude::*;
-mod pages;
+use std::{
+    io::prelude::*,
+    net::{TcpListener, TcpStream},
+};
 
-// The pages
-#[derive(Clone, Routable, PartialEq)]
-enum Route {
-    #[not_found]
-    #[at("/")]
-    Home,
-    #[at("/team")]
-    Team,
-    #[at("/software")]
-    Software,
-    #[at("/blog")]
-    Blog,
-}
-
-// Access to the pages
-fn switch(routes: Route) -> Html {
-    match routes {
-        Route::Home => pages::home::Site(),
-        Route::Team => pages::team::Site(),
-        Route::Software => pages::software::Site(),
-        Route::Blog => pages::blog::Site(),
-    }
-}
-
-// The header component
-#[function_component(Header)]
-fn header() -> Html {
-    html! {
-        <div class={classes!("Header")}>
-            <a href="/" id="Home">{"KeyCap Studios "}</a> <a href="/software" id="Software">{"Software "}</a> <a href="/team" id="devs">{"Devs "}</a> <a href="/blog" id="blog">{"Blog "}</a>
-        </div>
-    }
-}
-
-// The main app
-#[function_component]
-fn App() -> Html {
-    html! {
-        <>
-            <Header/>
-
-            <BrowserRouter>
-                <Switch<Route> render={switch} />
-            </BrowserRouter>
-        </>
-    }
-}
-
-// The render of the main app
 fn main() {
-    yew::Renderer::<App>::new().render();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0u8; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!(
+        "Request: {}", 
+        String::from_utf8_lossy(&buffer[..])
+    );
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+
+    stream.flush().unwrap();
 }
