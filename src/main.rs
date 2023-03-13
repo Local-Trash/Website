4173f8ac27da2ad1d9c828c981bd1987d0c07155
Loc@lTrash <tqdagboy@gmail.com>
2023-03-13 15:08:43 -0700

    starting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use std::{
     io::prelude::*,
-    net::{TcpListener, TcpStream},
+    net::{TcpListener, TcpStream}, ops::Deref,
 };
+mod threadpool;
+use threadpool::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    let pool = ThreadPool::new(10);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(move || {
+            handle_connection(stream);
+        })
     }
 }
 
@@ -18,14 +24,42 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    println!(
-        "Request: {}", 
-        String::from_utf8_lossy(&buffer[..])
-    );
+    let pages = [
+        Page::new("", include_str!("html/index.html"))
+    ];
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-
-    stream.write(response.as_bytes()).unwrap();
+    let mut response = false;
+    for page in &pages {
+        if buffer.starts_with(&page.url[..]) {
+            stream.write(&page.content).unwrap();
+            response = true;
+        }
+    }
+    if !response {
+        stream.write(&pages[0].content).unwrap();
+    }
 
     stream.flush().unwrap();
+}
+
+struct Page {
+    url: Vec<u8>,
+    content: Vec<u8>
+}
+
+impl Page {
+    fn new(url: &str, content: &str) -> Self {
+        Self {
+            url: format!("GET /{}", url).as_bytes().to_vec(),
+            content: format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", content.len(), content).as_bytes().to_vec(),
+        }
+    }
+}
+
+impl Deref for Page {
+    type Target = Page;
+
+    fn deref(&self) -> &Self::Target {
+        &self
+    }
 }
